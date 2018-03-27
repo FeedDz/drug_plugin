27d3e75355a2d0f96bfc418a08bc23b1a23f2f4f
BuildTools <unconfigured@null.spigotmc.org>
2018-03-27 00:25:59 -0400

    Commit by Colin

--- Colin Plugin/src/me/plugin/ListenerClass.java
+++ Colin Plugin/src/me/plugin/ListenerClass.java
@@ -3,8 +3,8 @@ package me.plugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
 
 public class ListenerClass implements Listener {
 
+	//.
 }
